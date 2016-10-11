20e23d6f7cc00561fdca1282d588bf5d8ea73230
alexeykopievskiy <alexeykopievskiy@gmail.com>
2016-10-11 16:23:18 +0300

    fix dependencies

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,6 +2,6 @@
   'use strict';
 
   angular
-    .module('agreggion', ['ngAnimate', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ngMaterial', 'toastr', 'ngResource']);
+    .module('agreggion', ['ngAnimate', 'ui.router', 'ngMaterial', 'ngResource']);
 
 })();
